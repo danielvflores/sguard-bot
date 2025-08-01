123f4726e7010b2af95796935cb7811753935205
Daniel Flores <datrixxacc@gmail.com>
2025-08-01 10:08:51 -0400

    Updates command loading path
    Fixes an issue where commands were not being loaded
    due to an incorrect path.
    
    Updates the path to correctly point to the command
    files.

--- src/utils/global/loadCommands.ts
+++ src/utils/global/loadCommands.ts
@@ -5,5 +5,5 @@ import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export async function loadCommands() {
-    await importx(path.resolve(__dirname, "../command/**/*.{ts,js}"));
+    await importx(path.resolve(__dirname, "../../command/**/*.{ts,js}"));
 }
